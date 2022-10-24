dfef69ea975e02132bf8ac0fcef4699f813382e5
Robert <lactoz22@gmail.com>
2022-10-24 14:56:02 -0500

    Added the Footer component to the app

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import { Routes, Route } from 'react-router-dom';
 import Home from './components/Home.js';
 import Header from './components/Header';
+import Footer from './components/Footer';
 
 
 function App() {
@@ -11,6 +12,8 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />}/>
       </Routes>
+
+      <Footer />
     </div>
   );
 }
